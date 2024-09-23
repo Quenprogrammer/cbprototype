ff9cc4fe1dfc8d681c81f3e146be222ec7fbb20f
QuenProgrammer <adamsadam3667@gmail.com>
2024-09-23 08:27:26 -0700

    tuesday update

---
+++ src/app/features/courses/web-programming/web-programming.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WebProgrammingComponent } from './web-programming.component';
+
+describe('WebProgrammingComponent', () => {
+  let component: WebProgrammingComponent;
+  let fixture: ComponentFixture<WebProgrammingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WebProgrammingComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WebProgrammingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/courses/web-programming/web-programming.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-programming',
+  standalone: true,
+  imports: [],
+  templateUrl: './web-programming.component.html',
+  styleUrl: './web-programming.component.scss'
+})
+export class WebProgrammingComponent {
+
+}
